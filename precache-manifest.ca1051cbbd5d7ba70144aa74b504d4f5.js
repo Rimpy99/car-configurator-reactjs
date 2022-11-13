26d75f2a04b8eef3c49d4ce3bc46e979c8c835c8
Paweł Wrona <85957282+Rimpy99@users.noreply.github.com>
2022-11-13 21:23:34 +0100

    Updates

---
+++ precache-manifest.ca1051cbbd5d7ba70144aa74b504d4f5.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "99c38b8b4d3397a8b399",
+    "url": "/car-configurator-reactjs/static/js/main.99c38b8b.chunk.js"
+  },
+  {
+    "revision": "70cb5e30dbb0ef654c81",
+    "url": "/car-configurator-reactjs/static/js/1.70cb5e30.chunk.js"
+  },
+  {
+    "revision": "500822c1ef6eb643580b",
+    "url": "/car-configurator-reactjs/static/js/runtime~main.500822c1.js"
+  },
+  {
+    "revision": "9dc32e964a95af059551ee08abc74a66",
+    "url": "/car-configurator-reactjs/static/media/macanlogo.9dc32e96.png"
+  },
+  {
+    "revision": "0a61ebc7c6119bae396be5bd69ecc736",
+    "url": "/car-configurator-reactjs/static/media/panameralogo.0a61ebc7.png"
+  },
+  {
+    "revision": "6d84252d9ef033317bd8558172b59126",
+    "url": "/car-configurator-reactjs/static/media/p911logo.6d84252d.png"
+  },
+  {
+    "revision": "6a34732485e0abed52fa84bf430f692d",
+    "url": "/car-configurator-reactjs/index.html"
+  }
+];
