b7c327aaac5ffc6653b7b88675b8751bf0615044
Paweł Wrona <85957282+Rimpy99@users.noreply.github.com>
2022-11-13 19:52:51 +0100

    Pages components + Routes created

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,24 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
+
+//PAGES
+import { HomePage } from './pages/HomePage';
+import { CarConfigurator } from './pages/CarConfigurator';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <BrowserRouter>
+        <Routes>
+          <Route path='/' element={<HomePage />}/>
+          <Route path='/home' element={<Navigate to='/'/>}/>
+          <Route path='/car-configurator-reactjs' element={<Navigate to='/'/>}/>
+          <Route path='/porsche911' element={<CarConfigurator />}/>
+          <Route path='/porschePanamera' element={<CarConfigurator />}/>
+          <Route path='/porscheMacan' element={<CarConfigurator />}/>
+        </Routes>
+      </BrowserRouter>
+    </>
   );
 }
 
