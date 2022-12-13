97d753083310503042ce2f417e7d5d832fd894e4
Paweł Wrona <85957282+Rimpy99@users.noreply.github.com>
2022-12-13 20:59:29 +0100

    Summary conditional display added

--- src/components/Summary/Summary.tsx
+++ src/components/Summary/Summary.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
+import { useState, useEffect } from 'react';
 import styled from 'styled-components';
-
 import { GrFormClose } from "react-icons/gr";
 
 type OptionObject = {
@@ -16,23 +16,42 @@ type Props = {
     options: OptionObject,
 }
 
+type Prices = {
+    model: number,
+    color: number,
+    rims: number,
+    dynamicLightsSystemPlus: number,
+    NightVisionAssist: number,
+    PremiumPackage: number,
+}
+
 export const Summary = ({car, color, rims, options}: Props) => {
 
     const { dynamicLightsSystemPlus, NightVisionAssist, PremiumPackage } = options;
 
-    let modelPrice = '';
+    let modelPrice = 0;
 
     switch(car){
         case 'carrera 911':
-            modelPrice = '106,100';
+            modelPrice = 106100;
             break;
         case 'macan':
-            modelPrice = '57,500';
+            modelPrice = 57500;
             break;
         case 'panamera':
-            modelPrice = '92,400';
+            modelPrice = 92400;
             break;
     }
+
+    const prices = {
+        model: modelPrice,
+        color: color !== 'white' ? 1100 : 0,
+        rims: rims ? 2450 : 0,
+        dynamicLightsSystemPlus: options.dynamicLightsSystemPlus ? 720 : 0,
+        NightVisionAssist: options.NightVisionAssist ? 1050 : 0,
+        PremiumPackage: options.PremiumPackage ? 4200 : 0,
+    };
+
 
     return(
         <>
@@ -40,27 +59,27 @@ export const Summary = ({car, color, rims, options}: Props) => {
             <table>
                 <tr>
                     <td>Porsche model {car[0].toUpperCase() + car.substring(1)}</td>
-                    <td>{modelPrice}</td>
+                    <td>{'$' + prices.model.toLocaleString("en-US")}</td>
                 </tr>
                 <tr>
                     <td>Custom color</td>
-                    <td>{color !== 'white' ? '1,100' : <GrFormClose />}</td>
+                    <td>{prices.color ? '$' + prices.color.toLocaleString("en-US") : <GrFormClose />}</td>
                 </tr>
                 <tr>
                     <td>Dark themed rims</td>
-                    <td>{rims ? '2,450' : <GrFormClose />}</td>
+                    <td>{prices.rims ? '$' + prices.rims.toLocaleString("en-US") : <GrFormClose />}</td>
                 </tr>
                 <tr>
                     <td>Dynamic Lights System Plus</td>
-                    <td>{dynamicLightsSystemPlus ? '720' : <GrFormClose />}</td>
+                    <td>{prices.dynamicLightsSystemPlus ? '$' + prices.dynamicLightsSystemPlus.toLocaleString("en-US") : <GrFormClose />}</td>
                 </tr>
                 <tr>
                     <td>Night Vision Assist</td>
-                    <td>{NightVisionAssist ? '1,050' : <GrFormClose />}</td>
+                    <td>{prices.NightVisionAssist ? '$' + prices.NightVisionAssist.toLocaleString("en-US") : <GrFormClose />}</td>
                 </tr>
                 <tr>
                     <td>Dynamic Lights System Plus</td>
-                    <td>{PremiumPackage ? '4,200' : <GrFormClose />}</td>
+                    <td>{prices.PremiumPackage ? '$' + prices.PremiumPackage.toLocaleString("en-US") : <GrFormClose />}</td>
                 </tr>
             </table>
         </>
